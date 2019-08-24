61326cf02b4b1a9ddbab5e834cd3e31aa9387ec1
Samantha Jeet <samanthajeet@gmail.com>
2019-08-23 18:11:53 -0600

    hacky sack

--- src/components/AdminNav/AdminNav.js
+++ src/components/AdminNav/AdminNav.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import createIcon from '../../images/create-icon.png'
 
 const NavigationContainer = styled.main`
   /* border: 1px solid red; */
   height: 100%;
+  background: #F4722B;
 `
 
 const Navigation = styled.nav`
@@ -11,17 +13,25 @@ const Navigation = styled.nav`
   flex-direction: column;
   width: 15rem;
 
+  li{
+    height: 2.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
   
   li:hover {
-    color: red;
+    color: #F4722B;
     cursor: pointer;
+    background: white;
   }
 `;
 
 let pages = [
   {
     name: "Create Post",
-    val: "createpost"
+    val: "createpost",
+    icon: createIcon
   },
   {
     name: "Post Manager",
@@ -54,17 +64,19 @@ class AdminNav extends Component {
     }
   }
   render() {
-    // console.log(this.props);
+    console.log(this.props.history.location.pathname);
+    let {pathname} = this.props.history.location
     let { showNav } = this.state;
     return (
       <NavigationContainer>
         {this.props.history.location.pathname.includes("/admin") ? (
           <div>
-            <button onClick={() => this.handleOpen()}>show</button>
+            {/* <button onClick={() => this.handleOpen()}>show</button> */}
             <Navigation id={showNav ? null : "no-show"} >
               <ul>
                 {pages.map(page => (
-                  <li key={page.val} onClick={() => this.goToPage(page.val)}>
+                  <li key={page.val} onClick={() => this.goToPage(page.val)} style={{background: pathname === '/admin/' + page.val ? 'white' : null, color:pathname === '/admin/' + page.val ? '#F4722B' : null }}>
+                  <img src={page.icon} alt=""/>
                     {page.name}
                   </li>
                 ))}
